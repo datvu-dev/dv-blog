ca8b3818fb1d97bdd0c24577720cf6ba6b7a2403
Dat Vu <dothanhdat.vu@mail.com>
2016-06-17 00:12:50 +1000

    installed mongodb

--- app.js
+++ app.js
@@ -1,69 +1,34 @@
-// $(document).ready(function() {
-  var http = require("http");
-  var mysql = require("mysql");
-  var net = require('net');
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
 
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
 
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/dv_blog';
 
-  // Console will print the message
-  console.log('Server running at http://127.0.0.1:8082/');
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
 
-  // $("#submit-btn").on('click', function() {
-    // Create a connection to the database.
-    var pool = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "dv_blog"
-    });
+    // Get the documents collection
+    var collection = db.collection('users');
 
-
-
-
-
-    pool.getConnection(function(err, connection){
+    // Insert some users
+    collection.find({name: 'modulus user'}).toArray(function (err, result) {
       if (err) {
-        throw err;
+        console.log(err);
+      } else if (result.length) {
+        console.log('Found:', result);
+      } else {
+        console.log('No document(s) found with defined "find" criteria!');
       }
-      connection.query('SELECT * FROM posts', function(err, rows) {
-        if (err) {
-          throw err;
-        }
-
-        http.createServer(function (request, response) {
-
-           // Send the HTTP header
-           // HTTP Status: 200 : OK
-           // Content Type: text/plain
-           response.writeHead(200, {'Content-Type': 'text/plain'});
-
-           console.log('this is a test');
-           response.end('finish');
-           // Send the response body as "Hello World"
-
-        }).listen(8082);
-      });
-
-      connection.release();
+      //Close connection
+      db.close();
     });
-
-
-
-    // con.connect(function(err) {
-    //   if (err) {
-    //     console.log('Error connecting to the database');
-    //     return;
-    //   }
-    //
-    //   console.log('Connection established');
-    // });
-
-    // console.log(con);
-
-
-    // con.end(function(err) {
-
-    // });
-  // });
-
-// });
+  }
+});
