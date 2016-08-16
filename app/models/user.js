64595c3aa1b1323ddfb3c617983c3afd562ebf14
Dat Vu <datvu@Dats-MacBook-Pro.local>
2016-08-16 16:54:17 +1000

    create new user model

---
+++ app/models/user.js
@@ -0,0 +1,24 @@
+// app/models/user.js
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define schema for user model
+var userSchema = mongoose.Schema({
+  local : {
+    email : String,
+    password : String
+  }
+});
+
+// generate a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// check if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
