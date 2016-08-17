9bdefb02d496aeff1093d523041df89959393754
Dat Vu <datvu@Dats-MacBook-Pro.local>
2016-08-17 23:58:39 +1000

    rebuild utility link component to accept modal parameter

--- public/js/components/utility-link.js
+++ public/js/components/utility-link.js
@@ -2,17 +2,21 @@ var Links = React.createClass({
   render: function() {
     return (
       <span>
-        <a className="utility-link" onClick={this.props.onEdit}><small>Edit</small></a>
+        <Link className="utility-link" to={{
+          pathname: this.props.path,
+          state: {modal: this.props.isModal}
+        }}><small>Edit</small></Link>
         <a className="utility-link" onClick={this.props.onDelete}><small>Remove</small></a>
       </span>
-    )    
+    )
   }
 });
 
 var UtilityLinks = React.createClass({
   render: function() {
     if (localStorage.getItem('user')) {
-      var linkItems = <Links onEdit={this.props.onEdit} onDelete={this.props.onDelete} />
+      var linkItems = <Links path={this.props.path} onDelete={this.props.onDelete}
+        isModal={this.props.isModal} />
     }
 
     return (
