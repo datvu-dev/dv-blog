331a92e6c3c5889e31908233b1df8bd4e9424882
Dat Vu <dothanhdat.vu@mail.com>
2016-08-30 11:39:34 +1000

    modify delete link component to accept parameters for text and class

--- public/js/components/delete-link.js
+++ public/js/components/delete-link.js
@@ -1,8 +1,11 @@
 var DeleteLink = React.createClass({
   render: function() {
+    var text = this.props.linkText ? this.props.linkText : 'Remove';
+    var className = this.props.linkClass ? this.props.linkClass : 'utility-link';
+
     if (localStorage.getItem('user')) {
-      var linkItem = <a className="utility-link"
-        onClick={this.props.onDelete}><small>Remove</small></a>
+      var linkItem = <a className={className}
+        onClick={this.props.onDelete}><small>{text}</small></a>
     }
 
     return (
