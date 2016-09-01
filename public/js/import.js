a33e7c65b503e9a2155e13d0846c9f14e9a42754
Dat Vu <dothanhdat.vu@mail.com>
2016-09-01 13:04:15 +1000

    change require to import

---
+++ public/js/import.js
@@ -0,0 +1,6 @@
+// public/js/import.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, IndexLink, Link, browserHistory} from 'react-router';
+import { WithContext as ReactTags } from 'react-tag-input';
