033662ff524f9ceab5e119ea8138fe6e3e5f0e43
Dat Vu <datvu@Dats-MacBook-Pro.local>
2016-09-21 15:23:38 +1000

    added production db config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 // config/database.js
 
+var local_db = 'mongodb://localhost:27017/dv_blog';
+var prod_db = 'mongodb://datvu:Developer123@ds035796.mlab.com:35796/dv-blog';
+
 module.exports = {
-    url : 'mongodb://localhost:27017/dv_blog'
+    url : prod_db
 };
