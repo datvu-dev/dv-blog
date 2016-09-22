ffd4eebd8918b6955ef032c4a4c1f0d81c62210a
Dat Vu <datvu@Dats-MacBook-Pro.local>
2016-09-23 00:03:41 +1000

    updated database.js to include mlab detail for database user

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 
 var local_db = 'mongodb://localhost:27017/dv_blog';
-var prod_db = 'mongodb://datvu:Developer123@ds035796.mlab.com:35796/dv-blog';
+var prod_db = 'mongodb://test:Test123@ds035796.mlab.com:35796/dv-blog';
 
 module.exports = {
     url : prod_db
